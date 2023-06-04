87d99e4697c4f409a1a9ae7bd62ef3dcbc8114b5
Krishna_D <dkrishna051997@gmail.com>
2023-06-04 22:01:36 +0530

    final Draft

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let menu=document.getElementsByClassName("container")[0];
+let place=document.getElementsByClassName("placed")[0];
 
 async function getMenu(){
     menu.innerHTML="";
     try{
-        let reponse=await fetch("https://sandeep836.github.io/json/food.json");
+        let reponse=await fetch("https://avivashishta29.github.io/f2-contest-test/db.json");
         let result =await reponse.json();
 
         for(let i=0;i<result.length;i++){
@@ -23,21 +24,44 @@ async function getMenu(){
         console.log("GetMenu Error", Error);
     }
 }
+let arr=[];
+
 
 
 // ---------------Take Order-----------
 function TakeOrder() {
+    place.innerHTML="";
     return new Promise((resolve) => {
       setTimeout(() => {
         const burgers = ['Cheeseburger', 'Pizza', 'Tacos', 'Sushi', 'Pasta', 'Fried Chicken', 'Grilled Cheese Sandwich', 'Steak', 'Caesar Salad', 'Fish and Chips', 'Ramen', 'Burrito', 'Pho', 'Pad Thai', 'Gyro', 'Ice Cream', 'Smoothie', 'Apple Pie', 'Chocolate Cake', 'Pancakes', 'Cupcake', 'Crepes', 'Club Sandwich', 'Falafel', 'Curry']
         const order = {
           burgers: burgers.sort(() => 0.5 - Math.random()).slice(0, 3),
         };
+        arr=order.burgers;
+        fetch('https://avivashishta29.github.io/f2-contest-test/db.json')
+         .then(response => response.json()).then(result =>{
+            for(let i=0;i<arr.length;i++){
+                for(let j=0;j<result.length;j++){
+                    if(arr[i]===result[j].name){
+                        let data=result[j];
+                        let placedOrder=document.createElement("div");
+                        let innerContent=`
+                        <p>${data.name}</p>
+                        <img src=${data.imgSrc} alt=${data.name}>
+                        <p>$${data.price}</P>`;
+                        
+                        placedOrder.className="placed-details";
+                        placedOrder.innerHTML=innerContent;
+                        place.append(placedOrder);
+                    }
+                }
+            }
+        })
         resolve(order);
       }, 2500);
     });
   }
-  
+    
   // Function for order preparation
   function orderPrep() {
     return new Promise((resolve) => {
@@ -62,18 +86,19 @@ function TakeOrder() {
   }
   
   // Function to handle the tasks sequentially using async/await
+
   async function place_order() {
     const order = await TakeOrder();
     console.log("Order:", order);
     const orderStatus = await orderPrep();
-    console.log("Order status:", orderStatus);
+    console.log("Order_Preparation status:", orderStatus);
   }
   
   // Run the pay flow
   async function pay(){
-    const paymentStatus = await payOrder();
-    console.log("Payment status:", paymentStatus);
-    thankyouFnc();
+        const paymentStatus = await payOrder();
+        console.log("Payment_Order status:", paymentStatus);
+        thankyouFnc();
   }
  
 
